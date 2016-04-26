ecab4a7b65dd2d14134e4336c99ae57bc610fdda
Bhargav Kowshik <bhargav@mapbox.com>
2016-04-26 12:36:27 +0530

    Add missing export geomertyTransformation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  'cityDeleted': require('./comparators/city-deleted')
+  'cityDeleted': require('./comparators/city-deleted'),
+  'geometryTransformation': require('./comparators/geometry-transformation')
 };
