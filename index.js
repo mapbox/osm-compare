93d5a11daaef4fb8087ee7ace84e293eedf6c3b3
Bhargav Kowshik <bhargav@mapbox.com>
2016-05-03 12:05:08 +0530

    Add compare functions to module exports

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 
 module.exports = {
   'cityDeleted': require('./comparators/city-deleted'),
-  'geometryTransformation': require('./comparators/geometry-transformation')
+  'geometryTransformation': require('./comparators/geometry-transformation'),
+  'compare_geometry': require('./comparators/compare_geometry'),
+  'compare_tag': require('./comparators/compare_tag'),
+  'place_significant': require('./comparators/place_significant'),
+  'user_changesets': require('./comparators/user_changesets'),
+  'count_tag': require('./comparators/count_tag'),
+  'delete_create': require('./comparators/delete_create'),
+  'landmark': require('./comparators/landmark'),
+  'city_deleted': require('./comparators/city_deleted'),
+  'compare_properties': require('./comparators/compare_properties'),
+  'compare_geometries': require('./comparators/compare_geometries'),
+  'highway_deleted': require('./comparators/highway_deleted')
 };
