05c39ab50021b25987c2151effa466d464ade205
Bhargav Kowshik <bhargav@mapbox.com>
2016-05-05 11:48:27 +0530

    Identify version of the feature

---
+++ comparators/feature_version.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = feature_version;
+
+/**
+* Identify version of the feature.
+* @param {object} newVersion New version of a feature in GeoJSON.
+* @param {object} oldVersion Old version of a feature in GeoJSON.
+* @param {function} callback Called with (error, result).
+* @returns {undefined} calls callback.
+*/
+function feature_version(newVersion, oldVersion, callback) {
+
+  if (!newVersion) {
+    return callback(null, {});
+  }
+
+  var version = newVersion.properties ? newVersion.properties['osm:version'] : newVersion['version'];
+  var result = {'result:feature_version': version};
+  callback(null, result);
+}
