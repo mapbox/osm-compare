0fc224f5f1459b2f2f6a34dbecebc5efc26c601b
Bhargav Kowshik <bhargav@mapbox.com>
2016-05-06 17:40:56 +0530

    Increase number of uncommon tags from 10 to 20

--- data/common-tags-from-taginfo.js
+++ data/common-tags-from-taginfo.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var keyValues = {
-  'admin_level': [8, 10, 6, 9, 7, 4, 2, 5, 11, 3],
-  'natural': ['water', 'tree', 'wood', 'wetland', 'scrub', 'coastline', 'peak', 'cliff', 'grassland', 'tree_row'],
-  'boundary': ['administrative', 'protected_area', 'Опорная␣точка', 'landuse', 'national_park', 'postal_code', 'political', 'marker', 'lot', 'cadastral'],
-  'highway': ['residential', 'service', 'track', 'unclassified', 'footway', 'path', 'tertiary', 'secondary', 'primary', 'crossing'],
-  'place': ['locality', 'hamlet', 'village', 'isolated_dwelling', 'island', 'suburb ', 'town', 'neighbourhood', 'islet', 'farm'],
-  'landuse': ['residential', 'forest', 'farmland', 'grass ', 'meadow', 'farmyard', 'farm', 'industrial', 'orchard', 'reservoir'],
-  'waterway': ['stream', 'ditch', 'river', 'drain', 'riverbank', 'canal', 'dam', 'weir', 'rapids', 'artificial']
+  'admin_level': ['8', '10', '6', '9', '7', '4', '2', '5', '11', '3', '12', '99', '0', 'A1', 'Provinsi', '1', 'A', 'Kabupaten', 'RW', 'kabupaten'],
+  'natural': ['water', 'tree', 'wood', 'wetland', 'scrub', 'coastline', 'peak', 'cliff', 'grassland', 'tree_row', 'heath', 'rock', 'bare_rock', 'beach', 'sand', 'spring', 'land', 'scree', 'bay', 'ridge'],
+  'boundary': ['administrative', 'protected_area', 'Опорная точка', 'landuse', 'national_park', 'postal_code', 'political', 'marker', 'lot', 'cadastral', 'forest_compartment', 'census', 'maritime', 'local_authority', 'religious_administration', 'civil_parish', 'forestry_compartment', 'natural', 'yes', 'historic'],
+  'highway': ['residential', 'service', 'track', 'unclassified', 'footway', 'path', 'tertiary', 'secondary', 'primary', 'crossing', 'bus_stop', 'turning_circle', 'cycleway', 'traffic_signals', 'living_street', 'trunk', 'motorway', 'steps', 'street_lamp', 'motorway_link'],
+  'place': ['locality', 'hamlet', 'village', 'isolated_dwelling', 'island', 'suburb', 'town', 'neighbourhood', 'islet', 'farm', 'city', 'allotments', 'county', 'city_block', 'plot', 'region', 'quarter', 'yes', 'state', 'municipality'],
+  'landuse': ['residential', 'forest', 'farmland', 'grass', 'meadow', 'farmyard', 'farm', 'industrial', 'orchard', 'reservoir', 'cemetery', 'vineyard', 'allotments', 'commercial', 'basin', 'quarry', 'retail', 'village_green', 'construction', 'brownfield'],
+  'waterway': ['stream', 'ditch', 'river', 'drain', 'riverbank', 'canal', 'dam', 'weir', 'rapids', 'artificial', 'waterfall', 'lock_gate', 'wadi', 'drystream', 'soakhole', 'dock', 'boatyard', 'milestone', 'derelict_canal', 'lock']
 };
 
 module.exports = keyValues;
