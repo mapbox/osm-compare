2246d2de44e6538faf0192d87a2d96b560e325d2
Arun Ganesh <arun.planemad@gmail.com>
2016-05-20 17:56:49 +0530

    Add feature deletion check

--- comparators/priority_area.js
+++ comparators/priority_area.js
@@ -17,11 +17,15 @@ module.exports = priority_area;
 */
 function priority_area(newVersion, oldVersion, callback) {
 
-  var priority_area_file = path.join(__dirname, '../data/priority_area.geojson');
+  // Exit if the feature has been deleted
+  if (!newVersion || !newVersion.geometry) return callback(null, {});
+
+  // Load the areas
+  var priority_area_file = path.join(__dirname, '../data/priority_areas.geojson');
   var priority_areas = JSON.parse(fs.readFileSync(priority_area_file, 'utf-8'));
 
   var result = Boolean(turfWithin(turfFC([turfCentroid(newVersion)]), priority_areas).features.length);
-  console.log('Result', result);
+  // console.log('Result', result); // DEBUG
 
   return callback(null, {
     'result:priority_area': {
