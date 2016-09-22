86d43c2db5bd6687b41e6d7aa7044382f45c2e02
amishas157 <amishas157@gmail.com>
2016-09-22 17:20:53 +0530

    Download updated landmarks db

--- scripts/download_landmarks.sh
+++ scripts/download_landmarks.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ ! -f landmarks.spatialite ]; then
   echo "Downloading landmarks database from S3 ...";
-  curl -s https://s3.amazonaws.com/vandalism-dynamosm-support/landmarks.spatialite -o landmarks.spatialite
+  curl -s https://s3.amazonaws.com/vandalism-dynamosm-support/landmarksupdate.spatialite -o landmarks.spatialite
 fi
