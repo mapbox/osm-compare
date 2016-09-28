38dbb7994e902ab814848627674a98259764ed12
amishas157 <amisha@mapbox.com>
2016-09-28 23:48:05 +0530

    Download db unconitionally

--- scripts/download_landmarks.sh
+++ scripts/download_landmarks.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-if [ ! -f landmarks.spatialite ]; then
   echo "Downloading landmarks database from S3 ...";
   curl -s https://s3.amazonaws.com/vandalism-dynamosm-support/landmarksupdate.spatialite -o landmarks.spatialite
-fi
