d4dd8c479b853cc53f0c83ae1357988e001e2bf9
amishas157 <amishas157@gmail.com>
2016-12-13 17:30:07 +0530

    Clean up

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 'use strict';
 
 module.exports = {
-  'cityDeleted': require('./comparators/city-deleted'),
-  'geometryTransformation': require('./comparators/geometry-transformation'),
-  'compare_geometry': require('./comparators/compare_geometry'),
-  'compare_tag': require('./comparators/compare_tag'),
-  'place_significant': require('./comparators/place_significant'),
-  'user_changesets': require('./comparators/user_changesets'),
-  'count_tag': require('./comparators/count_tag'),
-  'delete_create': require('./comparators/delete_create'),
   'landmark_score': require('./comparators/landmark_score'),
-  'city_deleted': require('./comparators/city_deleted'),
-  'compare_properties': require('./comparators/compare_properties'),
-  'compare_geometries': require('./comparators/compare_geometries'),
-  'highway_deleted': require('./comparators/highway_deleted'),
-  'new_mapper': require('./comparators/new_mapper'),
-  'feature_version': require('./comparators/feature_version'),
-  'uncommon_tags': require('./comparators/uncommon_tags'),
-  'low_zoom_features': require('./comparators/low_zoom_features'),
-  'user_blocks': require('./comparators/user_blocks'),
-  'dirty_word': require('./comparators/dirty_word'),
-  'priority_area': require('./comparators/priority_area'),
   'name_modified': require('./comparators/name_modified')
 };
