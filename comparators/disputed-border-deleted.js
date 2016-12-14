0caa19849ef4c1968d9fa156806033baec1ce478
maning <emmanuel.sambale@gmail.com>
2016-12-14 16:34:12 +0530

    first draft for deleted disputed borders

---
+++ comparators/disputed-border-deleted.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = disputedBorderDeleted;
+
+function disputedBorderDeleted(newVersion, oldVersion, callback) {
+  var result = {};
+  result['result:disputed_border_deleted'] = {};
+
+  if (!newVersion && !oldVersion) {
+  // None of old version or new Version present
+    return callback(null, {});
+  }
+  if (!newVersion && oldVersion) {
+  // Only old Version is present, which indicates feature has been deleted
+  /*
+    Comparing the tags
+    Creating a result object
+  */
+    if (oldVersion.properties && oldVersion.properties['disputed']) {
+      result['result:disputed_border_deleted'] = true;
+    }
+  } else { return callback(null, { }); }
+
+
+  callback(null, result);
+}
