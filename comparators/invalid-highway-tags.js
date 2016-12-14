076c5b6a0e9569dedc7ac1b7bdf698eae2288f37
Upendra Yadav <upendra@mapbox.com>
2016-12-14 15:47:38 +0530

    Added array of valid tags

--- comparators/invalid-highway-tags.js
+++ comparators/invalid-highway-tags.js
@@ -3,10 +3,54 @@ module.exports = invalidHighwayTags;
 
 function invalidHighwayTags(newVersion, oldVersion, callback) {
   var result = {};
-  if (newVersion && newVersion.properties && newVersion.properties['highway']) {
-    if (newVersion.properties.highway !== 'yes') {
-      result['result.invalidHighwayTags'] = true;
-    }
+  var validHighwayTags = [
+    'yes',
+    'motorway',
+    'trunk',
+    'primary',
+    'secondary',
+    'tertiary',
+    'unclassified',
+    'residential',
+    'service',
+    'motorway_link',
+    'trunk_link',
+    'primary_link',
+    'secondary_link',
+    'tertiary_link',
+    'living_street',
+    'pedestrian',
+    'track',
+    'bus_guideway',
+    'escape',
+    'raceway',
+    'road',
+    'footway',
+    'bridleway',
+    'steps',
+    'path',
+    'cycleway',
+    'proposed',
+    'construction',
+    'rest_area',
+    'services',
+    'bus_stop',
+    'crossing',
+    'elevator',
+    'emergency_access_point',
+    'give_way',
+    'mini_roundabout',
+    'motorway_junction',
+    'passing_place',
+    'speed_camera',
+    'street_lamp',
+    'stop',
+    'traffic_signals',
+    'turning_circle'
+  ];
+  if (newVersion && newVersion.properties && newVersion.properties['highway'] && validHighwayTags.indexOf(newVersion.properties.highway) === -1) {
+    result['result.invalidHighwayTags'] = true;
   }
   callback(null, result);
 }
+
