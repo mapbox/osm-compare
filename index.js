d3372573686e0e1f97988ca5296386c41a62ba27
amishas157 <amishas157@gmail.com>
2016-12-16 18:39:29 +0530

    Add all comparators in compare_all

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 
 module.exports = {
   'landmark_score': require('./comparators/landmark_score'),
-  'name_modified': require('./comparators/name_modified')
+  'name_modified': require('./comparators/name_modified'),
+  'disputed-border-tag-changed': require('./comparators/disputed-border-tag-changed'),
+  'disputed-border-deleted': require('./comparators/disputed-border-deleted'),
+  'invalid-highway-tags': require('./comparators/invalid-highway-tags'),
+  'large-building': require('./comparators/large-building'),
+  'major_road_changed': require('./comparators/major_road_changed')
 };
