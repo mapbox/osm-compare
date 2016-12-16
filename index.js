de62beafd0a983cf7091b66f1f4081a6ee27d3de
amishas157 <amishas157@gmail.com>
2016-12-16 18:52:33 +0530

    Add proper name

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 module.exports = {
   'landmark_score': require('./comparators/landmark_score'),
   'name_modified': require('./comparators/name_modified'),
-  'disputed-border-tag-changed': require('./comparators/disputed-border-tag-changed'),
-  'disputed-border-deleted': require('./comparators/disputed-border-deleted'),
-  'invalid-highway-tags': require('./comparators/invalid-highway-tags'),
-  'large-building': require('./comparators/large-building'),
+  'disputed_border_tag_changed': require('./comparators/disputed-border-tag-changed'),
+  'disputed_border_deleted': require('./comparators/disputed-border-deleted'),
+  'invalid_highway_tags': require('./comparators/invalid-highway-tags'),
+  'large_building': require('./comparators/large-building'),
   'major_road_changed': require('./comparators/major_road_changed')
 };
