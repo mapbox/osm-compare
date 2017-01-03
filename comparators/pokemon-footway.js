0071f726a7e8dcd257f760c576ef9529824101c1
Bhargav Kowshik <bhargav@mapbox.com>
2017-01-03 13:59:04 +0530

    Remove d3-queue dependency since we just have one item

--- comparators/pokemon-footway.js
+++ comparators/pokemon-footway.js
@@ -2,7 +2,6 @@
 
 var request = require('request');
 var moment = require('moment');
-var queue = require('d3-queue').queue;
 var sax = require('sax');
 
 module.exports = pokemonFootway;
@@ -10,7 +9,7 @@ module.exports = pokemonFootway;
 function getAccountCreated(userID, callback) {
   var url = ' http://api.openstreetmap.org/api/0.6/user/' + userID;
   request(url, function (error, response, body) {
-    if (error) console.log(String(error));
+    if (error) return callback(error);
 
     if (!error && response.statusCode === 200) {
       var accountCreated;
@@ -38,10 +37,11 @@ function pokemonFootway(newVersion, oldVersion, callback) {
   var newUserDate = moment([2016, 11, 23]);
 
   if (newVersion && newVersion.properties && newVersion.properties.highway === 'footway' && newVersion.properties['osm:version'] === 1) {
-    var q = queue(1);
-    q.defer(getAccountCreated, newVersion.properties['osm:uid']);
-    q.awaitAll(function (error, results) {
-      var accountCreated = results[0];
+    getAccountCreated(newVersion.properties['osm:uid'], function (error, accountCreated) {
+      if (error) {
+        console.log(String(error));
+        return callback(null, {});
+      }
       if (accountCreated.unix() >= newUserDate.unix()) {
         return callback(null, {
           'result:pokemonFootway': true,
