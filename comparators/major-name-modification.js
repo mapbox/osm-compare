2188f99c804e1f8aaa5141d789a550cb0b5fc65d
Bhargav Kowshik <bhargav@mapbox.com>
2017-01-06 15:54:34 +0530

    Move distance check to the top, easier to read

--- comparators/major-name-modification.js
+++ comparators/major-name-modification.js
@@ -9,7 +9,7 @@ function majorNameModification(newVersion, oldVersion, callback) {
   if (!newVersion || !newVersion.properties || !newVersion.properties.name) return callback(null, {});
   if (!oldVersion || !oldVersion.properties || !oldVersion.properties.name) return callback(null, {});
 
-  var distance = new Levenshtein(newVersion.properties.name, oldVersion.properties.name);
+  var distance = new Levenshtein(newVersion.properties.name, oldVersion.properties.name).distance;
   var length = oldVersion.properties.name.length;
   var modification = 100.0 * distance / length;
 
@@ -17,7 +17,7 @@ function majorNameModification(newVersion, oldVersion, callback) {
   if (modification >= 50) {
     return callback(null, {
       'result:major_name_modification': true,
-      'result:levenshtein_distance': distance.distance,
+      'result:levenshtein_distance': distance,
       'result:escalate': true
     });
   }
