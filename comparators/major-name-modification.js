f3cfa0c8e59620546f3d26992a334378049a06d9
Sajjad Anwar <me@sajjad.in>
2017-02-08 09:57:17 +0530

    fix tests

--- comparators/major-name-modification.js
+++ comparators/major-name-modification.js
@@ -17,6 +17,7 @@ function majorNameModification(newVersion, oldVersion, callback) {
   var modification = 100.0 * distance / length;
 
   // If modification is greater than 50%, it is a major name modification.
+  process.stderr.write('# version ' + newVersion.properties['osm:version']);
   if (modification >= 50 && newVersion.properties['osm:version'] > 10) {
     return callback(null, {
       'result:major_name_modification': true,
