1f80f7288b1e5779bdb89e4ab7e96b0aa1e8801a
manoharuss <manoharuss@gmail.com>
2017-02-22 23:22:46 +0530

    Have some kindness Circle

--- comparators/modified-place-wikidata.js
+++ comparators/modified-place-wikidata.js
@@ -3,24 +3,16 @@
 module.exports = modifiedPlaceWikidata;
 
 function modifiedPlaceWikidata(newVersion, oldVersion, callback) {
-
   if (!newVersion || !newVersion.properties) return callback(null, {});
   if (!oldVersion || !oldVersion.properties) return callback(null, {});
-
-
   // Assumption: Modifications to wikidata tags of places need further verification as these are important
   // Target: Finding suspicious edits that mistag wikidata value
-
   //This comparator will only look at modifications to Wikidata tags to features with place tag.
-
-  if ('wikidata' in oldVersion.properties && 'wikidata' in newVersion.properties && 'place' in oldVersion.properties && 'place' in newVersion.properties){
-
-  	if(oldVersion.properties.wikidata != newVersion.properties.wikidata){
-
-  		return callback(null, {
-  			'result:modifiedPlaceWikidata': true
-  		})
-  	}
-
+  if ('wikidata' in oldVersion.properties && 'wikidata' in newVersion.properties && 'place' in oldVersion.properties && 'place' in newVersion.properties) {
+    if (oldVersion.properties.wikidata !== newVersion.properties.wikidata) {
+      return callback(null, {
+        'result:modifiedPlaceWikidata': true
+      });
+    }
   }
-
+}
