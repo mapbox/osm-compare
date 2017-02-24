3aad1d4c9bbca112c138be50ffa17cc7ee16e7a7
Bhargav Kowshik <bhargav@mapbox.com>
2017-02-24 17:56:06 +0530

    Add comparator to package index

--- index.js
+++ index.js
@@ -18,5 +18,6 @@ module.exports = {
   'pokemon_edits': require('./comparators/pokemon_edits'),
   'dragged_highway_waterway': require('./comparators/dragged_highway_waterway'),
   'added_place': require('./comparators/added_place'),
-  'modified_place_wikidata': require('./comparators/modified-place-wikidata')
+  'modified_place_wikidata': require('./comparators/modified-place-wikidata'),
+  'osm_landmarks': require('./comparators/osm-landmarks'),
 };
