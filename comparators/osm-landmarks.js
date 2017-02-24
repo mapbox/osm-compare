c1c09310553680b1e859311f24c56187ba6083ee
Bhargav Kowshik <bhargav@mapbox.com>
2017-02-24 17:44:27 +0530

    Add feature type to result

--- comparators/osm-landmarks.js
+++ comparators/osm-landmarks.js
@@ -5,7 +5,8 @@ var getLakes = require('osm-landmarks').getLakes;
 module.exports = osmLandmarks;
 
 function osmLandmarks(newVersion, oldVersion, callback) {
-  var result = {'result:osm-landmark': false};
+  var result = {};
+
   var featureID = String(newVersion.properties['osm:id']);
   var featureType = String(newVersion.properties['osm:type']);
 
@@ -14,7 +15,10 @@ function osmLandmarks(newVersion, oldVersion, callback) {
 
     for (var i = 0; i < lakes.length; i++) {
       if (lakes[i][0] === featureID && lakes[i][1] === featureType) {
-        return callback(null, {'result:osm-landmark': true});
+        return callback(null, {
+          'result:osm-landmark': true,
+          'result:feature': 'lake'
+        });
       }
     }
 
