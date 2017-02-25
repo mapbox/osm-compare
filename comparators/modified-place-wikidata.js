17fd039bdd2293628e6f86fad65736d63a892710
manoharuss <manoharuss@gmail.com>
2017-02-25 17:33:22 +0530

    Added more cases

--- comparators/modified-place-wikidata.js
+++ comparators/modified-place-wikidata.js
@@ -5,9 +5,27 @@ module.exports = modifiedPlaceWikidata;
 function modifiedPlaceWikidata(newVersion, oldVersion, callback) {
   if (!newVersion || !newVersion.properties) return callback(null, {});
   if (!oldVersion || !oldVersion.properties) return callback(null, {});
-  // Assumption: Modifications to wikidata tags of places need further verification as these are important
+  // Assumption: Place features with edited wikidata tag need to flagged
   // Target: Finding suspicious edits that mistag wikidata value
   //This comparator will only look at modifications to Wikidata tags to features with place tag.
+
+
+  // This if condition handles for new additions of place tag for features that already have wikidata tag
+  if ('wikidata' in oldVersion.properties && !('place' in oldVersion.properties && 'wikidata' in newVersion.properties && 'place' in newVersion.properties)){
+    return callback(null, {
+      'result:modifiedPlaceWikidata': true
+    });
+  }
+  
+  // Handles when a place feature is given a new wikidata tag
+  if ('place' in oldVersion.properties && !('wikidata' in oldVersion.properties) && 'place' in newVersion.properties && 'wikidata' in newVersion.properties){
+    return callback(null, {
+      'result:modifiedPlaceWikidata': true
+    })
+
+  }
+
+  // Handles modifications to wikidata tag for features with place tag
   if ('wikidata' in oldVersion.properties && 'wikidata' in newVersion.properties && 'place' in oldVersion.properties && 'place' in newVersion.properties) {
     if (oldVersion.properties.wikidata !== newVersion.properties.wikidata) {
       return callback(null, {
