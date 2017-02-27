adfe1641cafedfaec13ab23aa6b556249d0b8103
Srividya <srividya@mapbox.com>
2017-02-27 18:20:23 +0530

    fixed linting errors

--- comparators/modifiedMonument.js
+++ comparators/modifiedMonument.js
@@ -3,13 +3,13 @@
 module.exports = modifiedMonument;
 
 function modifiedMonument(newVersion, oldVersion, callback) {
-	var result = {};
-	if (newVersion && newVersion.properties.hasOwnProperty('historic') && newVersion.properties.historic === 'monument' && newVersion.properties['osm:version'] > 10) {
-		result['result:modifiedMonument'] = true;
-	}
+  var result = {};
+  if (newVersion && newVersion.properties.hasOwnProperty('historic') && newVersion.properties.historic === 'monument' && newVersion.properties['osm:version'] > 10) {
+    result['result:modifiedMonument'] = true;
+  }
 
-	if (oldVersion && !newVersion && oldVersion.properties.hasOwnProperty('historic') && oldVersion.properties.historic === 'monument' && oldVersion.properties['osm:version'] > 10) {
-		result['result:modifiedMonument'] = true;
-	}
+  if (oldVersion && !newVersion && oldVersion.properties.hasOwnProperty('historic') && oldVersion.properties.historic === 'monument' && oldVersion.properties['osm:version'] > 10) {
+    result['result:modifiedMonument'] = true;
+  }
   callback(null, result);
 }
