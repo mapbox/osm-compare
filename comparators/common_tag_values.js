c9f825fa6bfba3e371ec1c385e1bd4b0a016701e
amishas157 <amishas157@gmail.com>
2017-03-07 14:49:09 +0530

    comparator for key value combination

---
+++ comparators/common_tag_values.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var fs = require('fs');
+
+module.exports = commonTagValues;
+
+var primaryTags = ['highway'];
+
+function commonTagValues(newVersion, oldVersion, callback) {
+  var result = {};
+
+  if (!newVersion || !newVersion.properties)
+    return callback(null, result);
+
+  for (var i = primaryTags.length - 1; i >= 0; i--) {
+    var tag = primaryTags[i];
+
+    if (tag in newVersion.properties) {
+      var data = fs.readFileSync('common_tag_values/' + tag + '.json');
+      var commonValues = JSON.parse(data.toString()).data;
+      var value = newVersion.properties[tag];
+      for (var j = commonValues.length - 1; j >= 0; j--) {
+        if (commonValues[j]['value'] === value && commonValues[j]['count'] > 10000) {
+          result['result:commonTagValues'] = true;
+        }
+      }
+    }
+  }
+
+  return callback(null, result);
+}
