11fe358c0a656bce5a2b0d96625d9c9df23de591
Bhargav Kowshik <bhargav@mapbox.com>
2017-03-22 15:38:08 +0530

    Add more water features to the list

--- comparators/water-feature-by-new-user.js
+++ comparators/water-feature-by-new-user.js
@@ -5,9 +5,16 @@ var request = require('request');
 module.exports = waterFeatureByNewUser;
 
 function isWaterFeature(feature) {
+  var landuse_values = ['pond', 'reservoir', 'salt_pond', 'basin'];
+
   var properties = feature.properties;
 
   if (properties.hasOwnProperty('natural') && properties['natural'] === 'water') return true;
+  if (properties.hasOwnProperty('water')) return true;
+  if (properties.hasOwnProperty('landuse') && (landuse_values.indexOf(properties['landuse']) !== -1)) return true;
+  if (properties.hasOwnProperty('reservoir_type') && properties['reservoir_type'] === 'water_storage') return true;
+  if (properties.hasOwnProperty('man_made') && properties['man_made'] === 'reservoir_covered') return true;
+  if (properties.hasOwnProperty('waterway')) return true;
 
   return false;
 }
