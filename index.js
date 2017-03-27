94d581e9a9044bbca9e0f655775dc5339bb1930b
Lukas Martinelli <me@lukasmartinelli.ch>
2017-03-27 16:11:28 -0400

    Convert large building to sync function

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
   'disputed_border_tag_changed': require('./comparators/disputed-border-tag-changed'),
   'disputed_border_deleted': require('./comparators/disputed-border-deleted'),
   'invalid_highway_tags': require('./comparators/invalid-highway-tags'),
-  'large_building': require('./comparators/large-building'),
+  'large_building': wrapsync(require('./comparators/large-building')),
   'major_road_changed': require('./comparators/major_road_changed'),
   'path_road_changed': require('./comparators/path_road_changed'),
   'major_lake_changed': require('./comparators/major-lake-modified'),
