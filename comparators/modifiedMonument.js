e551ac68e168c517bcef27f5842f0f6986b2e855
Lukas Martinelli <me@lukasmartinelli.ch>
2017-03-27 16:10:12 -0400

    Convert added modified monument to sync function

--- comparators/modifiedMonument.js
+++ comparators/modifiedMonument.js
@@ -2,7 +2,7 @@
 
 module.exports = modifiedMonument;
 
-function modifiedMonument(newVersion, oldVersion, callback) {
+function modifiedMonument(newVersion, oldVersion) {
   var result = {};
   if (newVersion && newVersion.properties.hasOwnProperty('historic') && newVersion.properties.historic === 'monument' && newVersion.properties['osm:version'] > 10) {
     result['result:modifiedMonument'] = true;
@@ -11,5 +11,5 @@ function modifiedMonument(newVersion, oldVersion, callback) {
   if (oldVersion && !newVersion && oldVersion.properties.hasOwnProperty('historic') && oldVersion.properties.historic === 'monument' && oldVersion.properties['osm:version'] > 10) {
     result['result:modifiedMonument'] = true;
   }
-  callback(null, result);
+  return result;
 }
