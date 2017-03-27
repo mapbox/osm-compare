1ca7427907fe527091ab95e1d833ff058aae16a5
Sajjad Anwar <me@sajjad.in>
2017-03-27 12:55:08 +0530

    better null island bbox

--- comparators/null-island.js
+++ comparators/null-island.js
@@ -10,7 +10,7 @@ function nullIsland(newVersion, oldVersion, callback) {
   if (!newVersion || !newVersion.hasOwnProperty('geometry') || newVersion['geometry'] === null) {
     return callback(null, {});
   }
-  var polygon = bboxPolygon([-6.722814455240666, -5.174241507358232, 6.570950888668989, 5.045358293553775]);
+  var polygon = bboxPolygon([-10.496769839987422, -4.291703357034322, 5.252754932388029, 4.291703357043673]);
   var center = centroid(newVersion);
   var inside = turfInside(center, polygon);
 
