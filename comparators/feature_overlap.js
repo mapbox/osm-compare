1876ed3c4d0a0471aa9d2d537731f90e6c0f9400
amishas157 <amishas157@gmail.com>
2017-03-31 11:48:09 +0530

    filter water bodies

--- comparators/feature_overlap.js
+++ comparators/feature_overlap.js
@@ -4,6 +4,7 @@ var featureCollection = require('../lib/feature-collection');
 var area = require('turf-area');
 var intersect  = require('turf-intersect');
 var cover = require('tile-cover');
+var featureFilter = require('feature-filter');
 
 var limits = {
   min_zoom: 16,
@@ -12,15 +13,29 @@ var limits = {
 
 module.exports = feature_overlap;
 
+var WaterwayJSON = [
+  'any',
+  ['==', 'natural', 'water'],
+  ['==', 'landuse', 'reservoir'],
+  ['==', 'natural', 'bay'],
+  ['==', 'waterway', 'dock'],
+  ['==', 'waterway', 'riverbank'],
+  ['==', 'natural', 'wetland']
+];
+var filterWaterway = featureFilter(WaterwayJSON);
+
 function feature_overlap(newVersion, oldVersion, callback) {
   var result = {};
-  if (newVersion && newVersion.geometry) {
+  if (newVersion && newVersion.geometry && newVersion.properties && filterWaterway(newVersion) && newVersion['properties']['osm:version'] === 1) {
     var tiles = cover.tiles(newVersion['geometry'], limits);
     featureCollection(tiles, function (err, result) {
       if (err)
         return callback(err);
       var overlaps = getOverLappingFeatures(newVersion, result);
-      return callback(null, {'result:feature_overlap': overlaps.length});
+      if (overlaps.length > 0)
+        return callback(null, {'result:feature_overlap': overlaps.length});
+      else
+        return callback();
     });
   } else {
     return callback(null, {});
