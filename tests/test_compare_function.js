c21f931323d3e92ae9e9fff476bb3ce0340b4ae0
amishas157 <amishas157@gmail.com>
2017-03-31 10:27:35 +0530

    dont use nock

--- tests/test_compare_function.js
+++ tests/test_compare_function.js
@@ -11,9 +11,9 @@ if (process.argv.length !== 3) {
   return;
 }
 
-nock('http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7')
-    .get('/16/46886/30383.mvt?access_token=pk.eyJ1IjoiYW1pc2hhIiwiYSI6ImNpcWt1bWc4bjAzOXNmdG04bng4dHVhZ3EifQ.bJK6rpjNmiP3kW2pROdScg')
-.reply(200, fs.readFileSync(path.join(__dirname, 'result_feature_overlap.pbf')));
+// nock('http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7')
+//     .get('/16/46886/30383.mvt?access_token=pk.eyJ1IjoiYW1pc2hhIiwiYSI6ImNpcWt1bWc4bjAzOXNmdG04bng4dHVhZ3EifQ.bJK6rpjNmiP3kW2pROdScg')
+// .reply(200, fs.readFileSync(path.join(__dirname, 'result_feature_overlap.pbf')));
 
 var filename = process.argv[2];
 var jsonData = JSON.parse(fs.readFileSync(path.join('.', filename), 'utf-8'));
