8d802b6d55975d8a7df75cfe2b43b418cd3e5542
Bhargav Kowshik <bhargav@mapbox.com>
2017-04-11 16:09:38 +0530

    Do not flag when all properties are removed

--- comparators/invalid_tag_modification.js
+++ comparators/invalid_tag_modification.js
@@ -22,6 +22,9 @@ function invalidTagModification(newVersion, oldVersion, callback) {
   var result = {};
   if (!newVersion || !oldVersion) return callback(null, result);
 
+  // If all properties of feature are removed, assume feature is being moved into a relation.
+  if (Object.keys(newVersion.properties).length === 0) return callback(null, {});
+
   var primaryTags = getPrimaryTags(oldVersion.properties);
   // Check if all primary tags are retained in newVersion.
   for (var i = 0; i < primaryTags.length; i++) {
