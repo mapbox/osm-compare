95c7698a242b3ec9028e50f9b4090b8483e52d46
Kushan Joshi <0o3ko0@gmail.com>
2017-04-12 15:10:21 +0530

    fix the test_compare_function

--- tests/test_compare_function.js
+++ tests/test_compare_function.js
@@ -6,32 +6,31 @@ var queue = require('queue-async');
 
 if (process.argv.length !== 3) {
   console.log('\nUsage: node test_compare_function.js fixture_filename\n');
-  throw new Error('usage error');
-}
-
-var filename = process.argv[2];
-var jsonData = JSON.parse(fs.readFileSync(path.join('.', filename), 'utf-8'));
-var compareFunctionPath = path.join(
-  '../',
-  'comparators',
-  jsonData.compareFunction
-);
-var compareFunction = require(compareFunctionPath);
-
-jsonData.fixtures.forEach(function(fixture) {
-  compareFunction(
-    fixture.newVersion,
-    fixture.oldVersion,
-    function(error, result) {
-      console.log(fixture.description);
-      console.log('expected', fixture.expectedResult);
-      if (error) console.log(error);
-      console.log('actual', JSON.stringify(result), '\n');
-      if (JSON.stringify(fixture.expectedResult) !== JSON.stringify(result)) {
-        console.log('Test failed! Actual is not expected!');
-      } else {
-        console.log('Test passed! Actual is same as expected');
-      }
-    }
+} else {
+  var filename = process.argv[2];
+  var jsonData = JSON.parse(fs.readFileSync(path.join('.', filename), 'utf-8'));
+  var compareFunctionPath = path.join(
+    '../',
+    'comparators',
+    jsonData.compareFunction
   );
-});
+  var compareFunction = require(compareFunctionPath);
+
+  jsonData.fixtures.forEach(function(fixture) {
+    compareFunction(
+      fixture.newVersion,
+      fixture.oldVersion,
+      function(error, result) {
+        console.log(fixture.description);
+        console.log('expected', fixture.expectedResult);
+        if (error) console.log(error);
+        console.log('actual', JSON.stringify(result), '\n');
+        if (JSON.stringify(fixture.expectedResult) !== JSON.stringify(result)) {
+          console.log('Test failed! Actual is not expected!');
+        } else {
+          console.log('Test passed! Actual is same as expected');
+        }
+      }
+    );
+  });
+}
