7b9272d5f07a9e36a268ec1426bbbc3994b5f9ed
Kushan Joshi <0o3ko0@gmail.com>
2017-05-10 14:52:11 +0530

    texting husky

--- data/low-zoom-features.js
+++ data/low-zoom-features.js
@@ -1,15 +1,15 @@
 'use strict';
 
 var lowZoomFeatures = {
-  'disputed': ['yes'],
-  'admin_level': [3, 2],
-  'natural': ['water', 'wood'],
-  'waterway': ['riverbank'],
-  'boundary': ['disputed', 'national_park'],
-  'highway': ['motorway', 'trunk'],
-  'place': ['continent', 'ocean', 'sea', 'country', 'state', 'city'],
-  'aeroway': ['aerodrome'],
-  'landuse': ['reservoir']
+  disputed: ['yes'],
+  admin_level: [3, 2],
+  natural: ['water', 'wood'],
+  waterway: ['riverbank'],
+  boundary: ['disputed', 'national_park'],
+  highway: ['motorway', 'trunk'],
+  place: ['continent', 'ocean', 'sea', 'country', 'state', 'city'],
+  aeroway: ['aerodrome'],
+  landuse: ['reservoir']
 };
 
 module.exports = lowZoomFeatures;
