f26b0f3166856c155ba5b8efc93e8a3b3cd107d7
Krishna Nammala <krishna@mapbox.com>
2017-05-16 11:29:30 +0530

    reduced the area to 2.5 sq km

--- comparators/large_building.js
+++ comparators/large_building.js
@@ -4,12 +4,16 @@ var turfArea = require('turf-area');
 module.exports = largeBuilding;
 
 function largeBuilding(newVersion, oldVersion) {
-  if (newVersion.deleted || !newVersion.hasOwnProperty('geometry') || newVersion['geometry'] === null) {
+  if (
+    newVersion.deleted ||
+    !newVersion.hasOwnProperty('geometry') ||
+    newVersion['geometry'] === null
+  ) {
     return false;
   }
   var area = turfArea(newVersion);
 
-  if (area > 5000000 && newVersion.properties.hasOwnProperty('building')) {
+  if (area > 2500000 && newVersion.properties.hasOwnProperty('building')) {
     return {'result:large_building': area};
   }
   return false;
