9e6f2698b33cd4f2eec3d95f9400c490efbcfd58
amishas157 <amishas157@gmail.com>
2017-07-26 15:13:40 +0530

    update large building cf

--- comparators/large_building.js
+++ comparators/large_building.js
@@ -4,17 +4,20 @@ var turfArea = require('turf-area');
 module.exports = largeBuilding;
 
 function largeBuilding(newVersion, oldVersion) {
-  if (
-    newVersion.deleted ||
-    !newVersion.hasOwnProperty('geometry') ||
-    newVersion['geometry'] === null
-  ) {
+  if (newVersion.deleted || !newVersion.geometry) {
     return false;
   }
-  var area = turfArea(newVersion);
 
-  if (area > 2500000 && newVersion.properties.hasOwnProperty('building')) {
-    return {'result:large_building': area};
+  if (newVersion.properties.hasOwnProperty('building')) {
+    const area = turfArea(newVersion);
+    if (newVersion.geometry.type !== 'MultiPolygon' && area > 500000) {
+      return {'result:large_building': true};
+    }
+    if (area > 2000000) {
+      return {'result:large_building': true};
+    }
+    return false;
   }
+
   return false;
 }
