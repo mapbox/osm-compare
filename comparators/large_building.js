ff3689d6675bf26f423c962c1a1e29a55c1815e7
Lukas Martinelli <me@lukasmartinelli.ch>
2017-07-26 06:16:28 -0400

    Add impossible angle tests to building

--- comparators/large_building.js
+++ comparators/large_building.js
@@ -1,23 +1,37 @@
 'use strict';
+const turfArea = require('turf-area');
+const impossibleAngle = require('./impossible_angle');
 
-var turfArea = require('turf-area');
 module.exports = largeBuilding;
 
+/*
+  Buildings with very large areas or large areas and weird angles.
+  @param {geojson} newVersion
+  @param {null} oldVersion
+  @returns {object|false) returns the detected incident or false
+*/
 function largeBuilding(newVersion, oldVersion) {
-  if (newVersion.deleted || !newVersion.geometry) {
-    return false;
+  if (newVersion.deleted || !newVersion.geometry) return false;
+  if (!newVersion.properties.hasOwnProperty('building')) return false;
+  const area = Math.round(turfArea(newVersion));
+  const hasImpossibleAngle = impossibleAngle(newVersion, oldVersion, {
+    maxAngle: 50
+  });
+  if (area > 500000 && hasImpossibleAngle) {
+    return {
+      'result:large_building': true,
+      message: `Building with large area ${area} and impossible angle ${hasImpossibleAngle.angle}`,
+      area: area,
+      angle: Math.round(hasImpossibleAngle.angle)
+    };
   }
 
-  if (newVersion.properties.hasOwnProperty('building')) {
-    const area = turfArea(newVersion);
-    if (newVersion.geometry.type !== 'MultiPolygon' && area > 500000) {
-      return {'result:large_building': true};
-    }
-    if (area > 2000000) {
-      return {'result:large_building': true};
-    }
-    return false;
+  if (area > 2000000) {
+    return {
+      'result:large_building': true,
+      message: `Building with very large area ${area}`,
+      area: area
+    };
   }
-
   return false;
 }
