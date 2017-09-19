ac11cd3c53f3f9dfc3ff78fe91ba026d165a4aa0
Wille Marcel <wille@wille.blog.br>
2017-09-19 11:16:55 -0300

    improve code and tests of modified_survey_point

--- comparators/modified_survey_point.js
+++ comparators/modified_survey_point.js
@@ -3,23 +3,37 @@
 module.exports = modifiedSurveyPoint;
 
 function modifiedSurveyPoint(newVersion, oldVersion) {
+  if (newVersion.deleted && !oldVersion) {
+    // None of old version or new Version present
+    return false;
+  }
+
+  // checks for changes
   if (
+    newVersion &&
     !newVersion.deleted &&
-    newVersion.properties['osm:version'] !== 1 &&
+    oldVersion &&
+    oldVersion.properties &&
     oldVersion.properties.hasOwnProperty('man_made') &&
     oldVersion.properties.man_made === 'survey_point'
   ) {
     if (
-      newVersion.geometry.coordinates[0] !==
+      newVersion.geometry &&
+      oldVersion.geometry &&
+      (newVersion.geometry.coordinates[0] !==
         oldVersion.geometry.coordinates[0] ||
-      newVersion.geometry.coordinates[1] !== oldVersion.geometry.coordinates[1]
+        newVersion.geometry.coordinates[1] !==
+          oldVersion.geometry.coordinates[1])
     ) {
+      // survey_point was moved
       return {'result:modified_survey_point': true};
     }
     if (
-      !newVersion.properties.hasOwnProperty('man_made') ||
-      newVersion.properties.man_made !== 'survey_point'
+      newVersion.properties &&
+      (!newVersion.properties.hasOwnProperty('man_made') ||
+        newVersion.properties.man_made !== 'survey_point')
     ) {
+      // survey_point lost its main tag
       return {'result:modified_survey_point': true};
     }
   }
@@ -27,6 +41,7 @@ function modifiedSurveyPoint(newVersion, oldVersion) {
   if (
     oldVersion &&
     newVersion.deleted &&
+    oldVersion.properties &&
     oldVersion.properties.hasOwnProperty('man_made') &&
     oldVersion.properties.man_made === 'survey_point'
   ) {
