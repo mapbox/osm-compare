90410191a0804688b8d1c2c77c21b38d13f705ad
manoharuss <manoharuss@gmail.com>
2017-10-26 15:36:26 +0530

    Added comparator for geometry edits to motorways and trunk roads

---
+++ comparators/motorway_trunk_geometry_changed.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var deepEquals = require('deep-equals');
+var MAJOR_ROAD_TYPES = ['motorway', 'trunk', 'motorway_link', 'trunk_link'];
+
+function getHighwayType(feature) {
+  return feature.properties.highway;
+}
+
+function isMajorRoad(feature) {
+  var highwayType = getHighwayType(feature);
+  return MAJOR_ROAD_TYPES.indexOf(highwayType) !== -1;
+}
+
+function getGeometry(feature) {
+  return feature.geometry;
+}
+
+function geometryChanged(newVersion, oldVersion) {
+  if (!oldVersion && newVersion.deleted) {
+    return false;
+  }
+
+  if (!oldVersion && !newVersion.deleted) {
+    return false;
+  }
+  if (isMajorRoad(oldVersion)) {
+    if (!deepEquals(getGeometry(oldVersion), getGeometry(newVersion))) {
+      return {
+        'result:major_road_changed': {
+          geometryChanged: true
+        }
+      };
+    }
+  }
+  return false;
+}
+
+module.exports = geometryChanged;
