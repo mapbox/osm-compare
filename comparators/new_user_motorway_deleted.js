c97cb871a2e50ccb0a461fec77e852ebbe056f8e
amishas157 <amishas157@gmail.com>
2018-02-22 20:43:08 +0530

    add new user motorway deleted detector
    Co-authored-by: manoharuss <manohar@mapbox.com>

---
+++ comparators/new_user_motorway_deleted.js
@@ -0,0 +1,39 @@
+'use strict';
+const newUser = require('./new_user');
+
+function isMotorway(newVersion, oldVersion) {
+  if (!oldVersion) {
+    return false;
+  }
+  if (newVersion.properties && newVersion.properties.highway) {
+    if (
+      newVersion.properties.highway === 'motorway' ||
+      newVersion.properties.highway === 'trunk' ||
+      newVersion.properties.highway === 'motorway_link' ||
+      newVersion.properties.highway === 'trunk_link'
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+function isDeleted(newVersion) {
+  if (newVersion.deleted) {
+    return true;
+  }
+  return false;
+}
+const isANewUser = (newVersion, oldVersion) =>
+  newUser(newVersion, oldVersion, {maxChangesets: 200});
+const newUserMotorwayDeleted = (newVersion, oldVersion) => {
+  if (
+    isANewUser(newVersion, oldVersion) &&
+    isMotorway(newVersion, oldVersion) &&
+    isDeleted(newVersion)
+  ) {
+    return {};
+  }
+  return false;
+};
+
+module.exports = newUserMotorwayDeleted;
