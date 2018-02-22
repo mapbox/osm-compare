8f21261141089e1e2a4c9b19cdb527da31b77457
amishas157 <amishas157@gmail.com>
2018-02-22 21:06:05 +0530

    Add test

--- comparators/new_user_footway.js
+++ comparators/new_user_footway.js
@@ -1,31 +1,33 @@
 'use strict';
 
 const newUser = require('./new_user');
-const impossibleAngles = require('./impossible-angle');
-const selfIntersecting = require('./self-intersecting');
-
+const impossibleAngles = require('../lib/impossible_angle');
+const selfIntersecting = require('../lib/self_intersecting');
 
 // check if the user is new user
 // check if the feature is actually a footway and it is version 1
 // check if the footway has imposible angle
 // check if the footway is selfintersecting
 
-
-
 function isFootway(newVersion) {
-  if (newVersion.properties && newVersion.properties.highway === 'footway' && newVersion.properties['osm:version'] === 1) {
+  if (
+    newVersion.properties &&
+    newVersion.properties.highway === 'footway' &&
+    newVersion.properties['osm:version'] === 1
+  ) {
     return true;
   }
   return false;
 }
 
 function isBadFootway(newVersion) {
-  return isFootway(newVersion) && (impossibleAngles(newVersion) || selfIntersecting(newVersion));
+  return isFootway(newVersion) &&
+    (impossibleAngles(newVersion) || selfIntersecting(newVersion));
 }
 
 const newUserFootway = (newVersion, oldVersion) => {
   if (newUser(newVersion, oldVersion) && isBadFootway(newVersion)) {
-    return {};
+    return {'result:new_user_footway': true};
   }
   return false;
 };
