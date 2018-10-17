90adeb1193e9df950c17ea925f1fa6e8a966c7e4
Wille Marcel <wille@wille.blog.br>
2018-10-17 10:10:46 -0300

    improve added_ref_tag comparator

--- comparators/added_ref_tag.js
+++ comparators/added_ref_tag.js
@@ -7,7 +7,13 @@ function addedPlace(newVersion, oldVersion) {
     return false;
   }
   if (oldVersion) {
-    if ('ref' in newVersion.properties && !('ref' in oldVersion.properties)) {
+    if (
+      'ref' in newVersion.properties &&
+      !('ref' in oldVersion.properties) &&
+      ('highway' in newVersion.properties ||
+        ('route' in newVersion.properties &&
+          newVersion.properties.route === 'road'))
+    ) {
       return {'result:added_ref_tag': true};
     }
   }
