53503db1f9de2d35fce5fc0b0af17a528c2c39cd
Wille Marcel <wille.yyz@gmail.com>
2021-05-14 18:59:32 -0300

    Don't raise error if we don't have user info

--- comparators/new_user.js
+++ comparators/new_user.js
@@ -1,27 +1,16 @@
 'use strict';
 module.exports = newUser;
 
-function ensureFeatureAugmented(ft) {
-  if (!('osm:user:changesetcount' in ft.properties)) {
-    throw new Error(
-      `Feature ${ft.id} is not augmented with required user information`
-    );
-  }
-}
-
 function newUser(newVersion, oldVersion, opts) {
   if (!newVersion || !newVersion.properties) {
     return false;
   }
-  ensureFeatureAugmented(newVersion);
   opts = Object.assign({maxChangesets: 50}, opts);
 
   const changesetCount = newVersion.properties['osm:user:changesetcount'];
-  const user = newVersion.properties['osm:user'];
 
-  if (changesetCount && changesetCount <= opts.maxChangesets) {
+  if (changesetCount > 0 && changesetCount <= opts.maxChangesets) {
     return {'result:new_user': true};
   }
-
   return false;
 }
